2f7e29bba0488b742ad0e9b9dbd0e01493941415
SUNDAYROO <164429837+SUNDAYROO@users.noreply.github.com>
2024-07-11 13:13:01 +0900

    Update VrayScene Exporter.py

--- VrayScene Exporter.py
+++ VrayScene Exporter.py
@@ -12,16 +12,6 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDialog, QLabel, QLineEdit,
-    QPushButton, QSizePolicy, QWidget)
-
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-    QMetaObject, QObject, QPoint, QRect,
-    QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QFileDialog, QStatusBar, QMessageBox, QComboBox, QDialog, QLabel, QLineEdit,
     QPushButton, QSizePolicy, QWidget)
 
